7672714df1d99fec836c5926b6dc3319a771993f
ForgetAndRemember <1965584294@qq.com>
2018-12-20 10:26:15 +0800

    the first commit

---
+++ Example/Pods/Target Support Files/Pods-WangXuAPIs_Tests/Pods-WangXuAPIs_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WangXuAPIs_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_WangXuAPIs_Tests
+@end
---
+++ Example/Pods/Target Support Files/WangXuAPIs/WangXuAPIs-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_WangXuAPIs : NSObject
+@end
+@implementation PodsDummy_WangXuAPIs
+@end
